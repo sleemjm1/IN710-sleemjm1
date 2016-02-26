53cb2afe891e1cfd2a94a66e7d8c950b0152a424
Joe <sleemjm1@student.op.ac.nz>
2016-02-26 17:16:28 +1300

    Added movie class

---
+++ Week Two/CollectionsAndExceptionHandling/CollectionsAndExceptionHandling/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsAndExceptionHandling
+{
+    class Movie
+    {
+        public int Year { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+
+        public Movie(int myYear, string myTitle, string myDirector)
+        {
+            Year = myYear;
+            Title = myTitle;
+            Director = myDirector;
+        }
+
+        public override string ToString()
+        {
+            return Year + "," + Title + ", " + Director;
+        }
+    }
+}
