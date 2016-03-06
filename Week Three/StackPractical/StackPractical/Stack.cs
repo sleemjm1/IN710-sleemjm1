38b12f2b610c85ec9a22f4fa745493e6c4938408
Joe <sleemjm1@student.op.ac.nz>
2016-03-06 19:48:57 +1300

    Stack exercise skeleton

---
+++ Week Three/StackPractical/StackPractical/Stack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackPractical
+{
+    class Stack
+    {
+        public void Stack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(string newString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEmpty()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
