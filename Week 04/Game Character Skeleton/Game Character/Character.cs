0e9e8062fb2cde260c9b697cd3bb877c072189fb
Joe <sleemjm1@student.op.ac.nz>
2016-03-08 22:50:03 +1300

    Character class added.

--- Week 04/Game Character Skeleton/Game Character/Character.cs
+++ Week 04/Game Character Skeleton/Game Character/Character.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Game_Character
 {
-    class Character
+    abstract class Character
     {
+        String Name;
+        ListBox listBox;
+        public Character(ListBox myListBox, String myName)
+        {
+            Name = myName;
+            listBox = myListBox;
+        }
     }
 }
