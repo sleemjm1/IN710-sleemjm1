0b44c8326615c5549dbf16273e24db382c2403e1
Joe <sleemjm1@student.op.ac.nz>
2016-03-08 22:18:35 +1300

    IWeapon, Sword, Character created.

---
+++ Week 04/Game Character Skeleton/Game Character/Sword.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Character
+{
+    public class Sword : IWeapon 
+    {
+        String UseWeapon()
+        {
+            return "I Slice my sword.";
+        }
+        
+    }
+}
