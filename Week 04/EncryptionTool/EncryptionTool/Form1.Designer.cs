8f3b0b16e1e9f27d58e855def166bb5dc553d8c5
Joe <sleemjm1@student.op.ac.nz>
2016-03-10 20:50:17 +1300

    Created form for encryption task

---
+++ Week 04/EncryptionTool/EncryptionTool/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace EncryptionTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbRot13 = new System.Windows.Forms.RadioButton();
+            this.rbReverse = new System.Windows.Forms.RadioButton();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbReverse);
+            this.groupBox1.Controls.Add(this.rbRot13);
+            this.groupBox1.Location = new System.Drawing.Point(48, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(168, 143);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ENCRYPTION";
+            // 
+            // rbRot13
+            // 
+            this.rbRot13.AutoSize = true;
+            this.rbRot13.Location = new System.Drawing.Point(25, 41);
+            this.rbRot13.Name = "rbRot13";
+            this.rbRot13.Size = new System.Drawing.Size(75, 21);
+            this.rbRot13.TabIndex = 0;
+            this.rbRot13.TabStop = true;
+            this.rbRot13.Text = "ROT13";
+            this.rbRot13.UseVisualStyleBackColor = true;
+            // 
+            // rbReverse
+            // 
+            this.rbReverse.AutoSize = true;
+            this.rbReverse.Location = new System.Drawing.Point(25, 84);
+            this.rbReverse.Name = "rbReverse";
+            this.rbReverse.Size = new System.Drawing.Size(82, 21);
+            this.rbReverse.TabIndex = 1;
+            this.rbReverse.TabStop = true;
+            this.rbReverse.Text = "Reverse";
+            this.rbReverse.UseVisualStyleBackColor = true;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(48, 231);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(514, 22);
+            this.txtInput.TabIndex = 1;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.BackColor = System.Drawing.SystemColors.Menu;
+            this.txtOutput.Enabled = false;
+            this.txtOutput.Location = new System.Drawing.Point(48, 283);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(514, 22);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(457, 158);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(105, 36);
+            this.btnEncrypt.TabIndex = 3;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 366);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Encryption";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbReverse;
+        private System.Windows.Forms.RadioButton rbRot13;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnEncrypt;
+    }
+}
+
