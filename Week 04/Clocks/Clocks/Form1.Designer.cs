5b971cabfd24486c265fa415ac472efaf7f0ec81
Joseph Sleeman (08003882) <SLEEMJM1@student.op.ac.nz>
2016-03-11 10:35:02 +1300

    Building form

---
+++ Week 04/Clocks/Clocks/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Clocks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbDigital = new System.Windows.Forms.RadioButton();
+            this.rbAnalogue = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(45, 49);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(194, 57);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(45, 125);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(194, 57);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbAnalogue);
+            this.groupBox1.Controls.Add(this.rbDigital);
+            this.groupBox1.Location = new System.Drawing.Point(289, 49);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(152, 133);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // rbDigital
+            // 
+            this.rbDigital.AutoSize = true;
+            this.rbDigital.Location = new System.Drawing.Point(22, 39);
+            this.rbDigital.Name = "rbDigital";
+            this.rbDigital.Size = new System.Drawing.Size(54, 17);
+            this.rbDigital.TabIndex = 0;
+            this.rbDigital.TabStop = true;
+            this.rbDigital.Text = "Digital";
+            this.rbDigital.UseVisualStyleBackColor = true;
+            // 
+            // rbAnalogue
+            // 
+            this.rbAnalogue.AutoSize = true;
+            this.rbAnalogue.Location = new System.Drawing.Point(22, 76);
+            this.rbAnalogue.Name = "rbAnalogue";
+            this.rbAnalogue.Size = new System.Drawing.Size(70, 17);
+            this.rbAnalogue.TabIndex = 1;
+            this.rbAnalogue.TabStop = true;
+            this.rbAnalogue.Text = "Analogue";
+            this.rbAnalogue.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 604);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Clocks";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbAnalogue;
+        private System.Windows.Forms.RadioButton rbDigital;
+    }
+}
+
