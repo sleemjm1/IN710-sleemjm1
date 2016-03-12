28a7617da70a29ece8f3764b10c0b9a3fd247394
Joe <sleemjm1@student.op.ac.nz>
2016-03-12 16:55:34 +1300

    Started work on Critter exercise

---
+++ Week 04/Animal Shelter Skeleton/IN710 4.1 Animal Shelter Solution 2014/TextDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IN710_4._1_Animal_Shelter_Solution_2014
+{
+    class TextDisplay: IDisplay
+    {
+        public void displayCritterList(List<Critter> critterList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void clearDisplay()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
