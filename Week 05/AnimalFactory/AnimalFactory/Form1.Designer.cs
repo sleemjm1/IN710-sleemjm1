e546bb34d656549b37b73f862c894bfd3975bcab
Joseph Sleeman (08003882) <SLEEMJM1@student.op.ac.nz>
2016-03-15 16:51:10 +1300

    Form stuff

--- Week 05/AnimalFactory/AnimalFactory/Form1.Designer.cs
+++ Week 05/AnimalFactory/AnimalFactory/Form1.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listDisplay = new System.Windows.Forms.ListBox();
+            this.btnNorthAmerica = new System.Windows.Forms.Button();
+            this.btnAustralia = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listDisplay
+            // 
+            this.listDisplay.FormattingEnabled = true;
+            this.listDisplay.Location = new System.Drawing.Point(244, 12);
+            this.listDisplay.Name = "listDisplay";
+            this.listDisplay.Size = new System.Drawing.Size(346, 420);
+            this.listDisplay.TabIndex = 0;
+            // 
+            // btnNorthAmerica
+            // 
+            this.btnNorthAmerica.Location = new System.Drawing.Point(244, 457);
+            this.btnNorthAmerica.Name = "btnNorthAmerica";
+            this.btnNorthAmerica.Size = new System.Drawing.Size(149, 23);
+            this.btnNorthAmerica.TabIndex = 1;
+            this.btnNorthAmerica.Text = "North America";
+            this.btnNorthAmerica.UseVisualStyleBackColor = true;
+            // 
+            // btnAustralia
+            // 
+            this.btnAustralia.Location = new System.Drawing.Point(441, 457);
+            this.btnAustralia.Name = "btnAustralia";
+            this.btnAustralia.Size = new System.Drawing.Size(149, 23);
+            this.btnAustralia.TabIndex = 2;
+            this.btnAustralia.Text = "Australia";
+            this.btnAustralia.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 524);
+            this.Controls.Add(this.btnAustralia);
+            this.Controls.Add(this.btnNorthAmerica);
+            this.Controls.Add(this.listDisplay);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listDisplay;
+        private System.Windows.Forms.Button btnNorthAmerica;
+        private System.Windows.Forms.Button btnAustralia;
     }
 }
 
