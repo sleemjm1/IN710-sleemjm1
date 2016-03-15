2c67c65471dfa29af1281594301bdeb0187a9416
Joseph Sleeman (08003882) <SLEEMJM1@student.op.ac.nz>
2016-03-15 16:43:24 +1300

    Added classes for Australia

---
+++ Week 05/AnimalFactory/AnimalFactory/Crocodile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalFactory
+{
+    class Crocodile : Animal
+    {
+        public Crocodile()
+        {
+            name = "Crocodile";
+            family = "Carnivore";
+            food = "Chickens";
+            image = new System.Drawing.Bitmap("crocodile.jpg");
+        }
+    }
+}
---
+++ Week 05/AnimalFactory/AnimalFactory/Kangaroo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalFactory
+{
+    class Kangaroo : Animal
+    {
+        public Kangaroo()
+        {
+            name = "Kangaroo";
+            family = "Herbavore";
+            food = "Grass";
+            image = new System.Drawing.Bitmap("kangaroo.jpg");
+        }
+    }
+}
