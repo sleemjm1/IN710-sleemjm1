409e0a8340ee464362ba0a84c250ffb61e3a4e4c
Joseph Sleeman (08003882) <SLEEMJM1@student.op.ac.nz>
2016-03-18 20:33:47 +1300

    Started but it's all wrong

---
+++ Week 05/MixAndMatch/MixAndMatch/BodyPart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixAndMatch
+{
+    public class BodyPart
+    {
+        protected Bitmap image;
+        public Bitmap Image
+        {
+            get { return image; } // read only
+        }
+    }
+}
---
+++ Week 05/MixAndMatch/MixAndMatch/WaltBody.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixAndMatch
+{
+    public class WaltBody : Body
+    {
+        public WaltBody()
+        {
+            image = new System.Drawing.Bitmap("walt_body.png");
+        }
+    }
+}
