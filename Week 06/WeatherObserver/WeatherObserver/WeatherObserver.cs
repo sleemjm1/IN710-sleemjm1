b2040dc7ba01c66219b593e8273808166afca2ae
Joe <sleemjm1@student.op.ac.nz>
2016-03-23 20:16:30 +1300

    Basic structure of Weather Observer

---
+++ Week 06/WeatherObserver/WeatherObserver/WeatherObserver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherObserver
+{
+    public abstract class WeatherObserver : IObserver
+    {
+        protected int currTemperature;
+        protected int currHumidity;
+        protected int currPressure;
+        protected double currComputedValue;
+        protected ListBox listBox;
+        protected WeatherSubject weatherSubject;
+
+        public abstract void Update(int data);
+
+        public void Display()
+        {
+            listBox.Items.Add(currComputedValue.ToString("F2"));
+        }
+
+        public WeatherObserver(ListBox listBox, WeatherSubject weatherSubject)
+        {
+            this.listBox = listBox;
+
+            currTemperature = 0;
+            currPressure = 0;
+            currHumidity = 0;
+
+            this.weatherSubject = weatherSubject;
+
+            weatherSubject.AddObserver(this);
+        }
+    }
+}
---
+++ Week 06/WeatherObserver/WeatherObserver/WeatherSubject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherObserver
+{
+    class WeatherSubject : ISubject
+    {
+        private List<IObserver> observerList;
+        private int currTemperature;
+        private int currHumidity;
+        private int currPressure;
+        public void AddObserver(IObserver o)
+        {
+            observerList.Add(o);
+        }
+
+        public void RemoveObserver(IObserver o)
+        {
+            observerList.Remove(o);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (WeatherObserver currObserver in observerList)
+            {
+                currObserver.Update(currTemperature, currHumidity, currPressure);
+            }
+        }
+
+        public void TriggerNotifications(TextBox tbTemperature, TextBox tbHumidity, TextBox tbPressure)
+        {
+            try
+            {
+                currTemperature = Int32.Parse(tbTemperature.Text);
+                currPressure = Int32.Parse(tbPressure.Text);
+                currHumidity = Int32.Parse(tbHumidity.Text);
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Please enter numerical values in all provided fields");
+            }
+        }
+    }
+}
