629ab0be28dffe5fe9d0c7790d8b1fe97b95dca3
Joe <sleemjm1@student.op.ac.nz>
2016-03-25 18:41:55 +1300

    Predictions working

--- Week 06/WeatherObserver/WeatherObserver/ForecastObserver.cs
+++ Week 06/WeatherObserver/WeatherObserver/ForecastObserver.cs
@@ -9,34 +9,88 @@ namespace WeatherObserver
 {
     public class ForecastObserver : WeatherObserver
     {
-        string heat;
         string dryness;
-        string pressure;
+        int? previousPressure;  // nullable int
+        string prediction;
 
         public ForecastObserver(ListBox listBox, WeatherSubject weatherSubject) : base(listBox, weatherSubject)
         {
-
+            previousPressure = null;
         }
 
         // In this Update method, instead of writing to currComputedTemperature, etc. In the base class, we will be writing to
         // Our local variables instead. This is because we will be using them in our overridden Display method below
+        // This update should be based entirely on the change in barometric pressure:
+        //          Over 102:
+        //                      Rising:             Continued fair
+        //                      Slowly Falling:     Fair
+        //                      Rapidly Falling:    Cloudy, warmer
+        //          100-102:    
+        //                      Rising:             Same as present
+        //                      Slowly Falling:     Little change
+        //                      Rapidly Falling:    Precipitation likely
+        //          Below 100:  
+        //                      Rising:             Clearing cooler
+        //                      Slowly Falling:     Precipitation
+        //                      Rapidly Falling:    Storm
         public override void Update(int currTemperature, int currHumidity, int currPressure)    
         {
-            if (currTemperature > 30)
-                heat = "Hot";
-            else if (currTemperature > 20 && currTemperature < 30)
-                heat = "Warm";
+            string state;
+            currComputedTemperature = currTemperature;  // will use this when displaying
+
+            if (previousPressure == null)
+            {
+                previousPressure = currPressure;
+                // we will make up the forecast as we have no way of telling
+                prediction = "asteriods will hit the earth.";
+            }
             else
-                heat = "Cold";
+            {
+                int difference = currPressure - (int)previousPressure;  // if this value is negative - we are falling
+
+                if (difference < 0 && difference > -2)                  // slowly falling 
+                    state = "Slowly Falling";
+                else if (difference < -2)                               // rapidly falling
+                    state = "Rapidly Falling";
+                else                                                    // else we must be rising
+                    state = "Rising";
+
+                switch (state)
+                {
+                    case "Rising":
+                        if (currPressure > 102)
+                            prediction = "continued fair weather";          // Rising  Continued fair weather
+                        else if (currPressure < 102 && currPressure <100)
+                            prediction = "not likely to change";            // Same as present
+                        else
+                            prediction = "clearing and cooler";             // Clearing cooler
+                        break;
+                    case "Slowly Falling":
+                        if (currPressure > 102)
+                            prediction = "fair weather"; // Fair
+                        else if (currPressure < 102 && currPressure <100)
+                            prediction = "little change expected";          // Little change
+                        else
+                            prediction = "showers forming";                 // Precipitation
+                        break;
+                    case "Rapidly Falling":
+                        if (currPressure > 102)
+                            prediction = "warmer weather but cloudy";       // Cloudy, Warmer
+                        else if (currPressure < 102 && currPressure <100)
+                            prediction = "showers likely, perhaps hail";    // Precipitation likely
+                        else
+                            prediction = "stormy weather expected";         // Storm
+                        break;
+                }   // end switch
+                
+            }
 
             if (currHumidity > 70)
-                dryness = "Humid";
+                dryness = "very humid";
             else if (currHumidity > 50 && currHumidity < 70)
-                dryness = "Average";
+                dryness = "humid";
             else
-                dryness = "Dry";
-
-            pressure = "High";  // I don't know anything about barometric pressure -- revisit
+                dryness = "dry";
         }
 
 
@@ -45,9 +99,9 @@ namespace WeatherObserver
         public override void Display()
         {
             listBox.Items.Clear();
-            listBox.Items.Add("You can expect a " + heat + " temperature.");
-            listBox.Items.Add("Humidity should be " + dryness + ".");
-            listBox.Items.Add("You can expect " + pressure + " pressure.");
+            listBox.Items.Add("You can expect a high of " + currComputedTemperature + " degrees.");
+            listBox.Items.Add("It should be a " + dryness + " day.");
+            listBox.Items.Add("Forecast prediction: " + prediction);
         }
 
     }
