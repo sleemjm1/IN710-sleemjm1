41c9c99e1dbaa9ba67e640fc9bc9f6e1969a71aa
Joe <sleemjm1@student.op.ac.nz>
2016-03-26 20:54:06 +1300

    Started social media task

---
+++ Week 06/SocialMedia/SocialMedia/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace SocialMedia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabMainPage = new System.Windows.Forms.TabPage();
+            this.tabFriend1 = new System.Windows.Forms.TabPage();
+            this.tabFriend2 = new System.Windows.Forms.TabPage();
+            this.tabFriend3 = new System.Windows.Forms.TabPage();
+            this.tabFriend4 = new System.Windows.Forms.TabPage();
+            this.listMainPage = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnUpdateStatus = new System.Windows.Forms.Button();
+            this.listFriend1 = new System.Windows.Forms.ListBox();
+            this.listFriend2 = new System.Windows.Forms.ListBox();
+            this.listFriend3 = new System.Windows.Forms.ListBox();
+            this.listFriend4 = new System.Windows.Forms.ListBox();
+            this.tabControl1.SuspendLayout();
+            this.tabMainPage.SuspendLayout();
+            this.tabFriend1.SuspendLayout();
+            this.tabFriend2.SuspendLayout();
+            this.tabFriend3.SuspendLayout();
+            this.tabFriend4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabMainPage);
+            this.tabControl1.Controls.Add(this.tabFriend1);
+            this.tabControl1.Controls.Add(this.tabFriend2);
+            this.tabControl1.Controls.Add(this.tabFriend3);
+            this.tabControl1.Controls.Add(this.tabFriend4);
+            this.tabControl1.Location = new System.Drawing.Point(2, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(736, 561);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabMainPage
+            // 
+            this.tabMainPage.BackColor = System.Drawing.SystemColors.Control;
+            this.tabMainPage.Controls.Add(this.btnUpdateStatus);
+            this.tabMainPage.Controls.Add(this.textBox1);
+            this.tabMainPage.Controls.Add(this.listMainPage);
+            this.tabMainPage.Location = new System.Drawing.Point(4, 22);
+            this.tabMainPage.Name = "tabMainPage";
+            this.tabMainPage.Padding = new System.Windows.Forms.Padding(3);
+            this.tabMainPage.Size = new System.Drawing.Size(728, 535);
+            this.tabMainPage.TabIndex = 0;
+            this.tabMainPage.Text = "Main Page";
+            // 
+            // tabFriend1
+            // 
+            this.tabFriend1.BackColor = System.Drawing.SystemColors.Control;
+            this.tabFriend1.Controls.Add(this.listFriend1);
+            this.tabFriend1.Location = new System.Drawing.Point(4, 22);
+            this.tabFriend1.Name = "tabFriend1";
+            this.tabFriend1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabFriend1.Size = new System.Drawing.Size(728, 535);
+            this.tabFriend1.TabIndex = 1;
+            this.tabFriend1.Text = "Friend 1";
+            // 
+            // tabFriend2
+            // 
+            this.tabFriend2.BackColor = System.Drawing.SystemColors.Control;
+            this.tabFriend2.Controls.Add(this.listFriend2);
+            this.tabFriend2.Location = new System.Drawing.Point(4, 22);
+            this.tabFriend2.Name = "tabFriend2";
+            this.tabFriend2.Size = new System.Drawing.Size(728, 535);
+            this.tabFriend2.TabIndex = 2;
+            this.tabFriend2.Text = "Friend 2";
+            // 
+            // tabFriend3
+            // 
+            this.tabFriend3.BackColor = System.Drawing.SystemColors.Control;
+            this.tabFriend3.Controls.Add(this.listFriend3);
+            this.tabFriend3.Location = new System.Drawing.Point(4, 22);
+            this.tabFriend3.Name = "tabFriend3";
+            this.tabFriend3.Size = new System.Drawing.Size(728, 535);
+            this.tabFriend3.TabIndex = 3;
+            this.tabFriend3.Text = "Friend 3";
+            // 
+            // tabFriend4
+            // 
+            this.tabFriend4.BackColor = System.Drawing.SystemColors.Control;
+            this.tabFriend4.Controls.Add(this.listFriend4);
+            this.tabFriend4.Location = new System.Drawing.Point(4, 22);
+            this.tabFriend4.Name = "tabFriend4";
+            this.tabFriend4.Size = new System.Drawing.Size(728, 535);
+            this.tabFriend4.TabIndex = 4;
+            this.tabFriend4.Text = "Friend 4";
+            // 
+            // listMainPage
+            // 
+            this.listMainPage.FormattingEnabled = true;
+            this.listMainPage.Location = new System.Drawing.Point(46, 36);
+            this.listMainPage.Name = "listMainPage";
+            this.listMainPage.Size = new System.Drawing.Size(624, 394);
+            this.listMainPage.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(201, 468);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(469, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnUpdateStatus
+            // 
+            this.btnUpdateStatus.Location = new System.Drawing.Point(46, 468);
+            this.btnUpdateStatus.Name = "btnUpdateStatus";
+            this.btnUpdateStatus.Size = new System.Drawing.Size(133, 23);
+            this.btnUpdateStatus.TabIndex = 2;
+            this.btnUpdateStatus.Text = "Update Status";
+            this.btnUpdateStatus.UseVisualStyleBackColor = true;
+            // 
+            // listFriend1
+            // 
+            this.listFriend1.FormattingEnabled = true;
+            this.listFriend1.Location = new System.Drawing.Point(52, 70);
+            this.listFriend1.Name = "listFriend1";
+            this.listFriend1.Size = new System.Drawing.Size(624, 394);
+            this.listFriend1.TabIndex = 1;
+            // 
+            // listFriend2
+            // 
+            this.listFriend2.FormattingEnabled = true;
+            this.listFriend2.Location = new System.Drawing.Point(52, 70);
+            this.listFriend2.Name = "listFriend2";
+            this.listFriend2.Size = new System.Drawing.Size(624, 394);
+            this.listFriend2.TabIndex = 1;
+            // 
+            // listFriend3
+            // 
+            this.listFriend3.FormattingEnabled = true;
+            this.listFriend3.Location = new System.Drawing.Point(52, 70);
+            this.listFriend3.Name = "listFriend3";
+            this.listFriend3.Size = new System.Drawing.Size(624, 394);
+            this.listFriend3.TabIndex = 1;
+            // 
+            // listFriend4
+            // 
+            this.listFriend4.FormattingEnabled = true;
+            this.listFriend4.Location = new System.Drawing.Point(52, 70);
+            this.listFriend4.Name = "listFriend4";
+            this.listFriend4.Size = new System.Drawing.Size(624, 394);
+            this.listFriend4.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 562);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabMainPage.ResumeLayout(false);
+            this.tabMainPage.PerformLayout();
+            this.tabFriend1.ResumeLayout(false);
+            this.tabFriend2.ResumeLayout(false);
+            this.tabFriend3.ResumeLayout(false);
+            this.tabFriend4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabMainPage;
+        private System.Windows.Forms.Button btnUpdateStatus;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listMainPage;
+        private System.Windows.Forms.TabPage tabFriend1;
+        private System.Windows.Forms.ListBox listFriend1;
+        private System.Windows.Forms.TabPage tabFriend2;
+        private System.Windows.Forms.ListBox listFriend2;
+        private System.Windows.Forms.TabPage tabFriend3;
+        private System.Windows.Forms.ListBox listFriend3;
+        private System.Windows.Forms.TabPage tabFriend4;
+        private System.Windows.Forms.ListBox listFriend4;
+    }
+}
+
