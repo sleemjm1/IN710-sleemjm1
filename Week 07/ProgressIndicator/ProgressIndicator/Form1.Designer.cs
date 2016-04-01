f8d53370a10dd48b7d50364f1749c2a7a5dc2097
Joseph Sleeman (08003882) <SLEEMJM1@student.op.ac.nz>
2016-04-01 17:23:32 +1300

    First delegate task complete

---
+++ Week 07/ProgressIndicator/ProgressIndicator/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace ProgressIndicator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.rbNumericSpin = new System.Windows.Forms.RadioButton();
+            this.rbProgressBar = new System.Windows.Forms.RadioButton();
+            this.rbTrackBar = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbTrackBar);
+            this.groupBox1.Controls.Add(this.rbProgressBar);
+            this.groupBox1.Controls.Add(this.rbNumericSpin);
+            this.groupBox1.Location = new System.Drawing.Point(288, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 145);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Progress Type";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(34, 137);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(34, 178);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(454, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(34, 223);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(454, 45);
+            this.trackBar1.TabIndex = 4;
+            // 
+            // rbNumericSpin
+            // 
+            this.rbNumericSpin.AutoSize = true;
+            this.rbNumericSpin.Location = new System.Drawing.Point(17, 34);
+            this.rbNumericSpin.Name = "rbNumericSpin";
+            this.rbNumericSpin.Size = new System.Drawing.Size(88, 17);
+            this.rbNumericSpin.TabIndex = 0;
+            this.rbNumericSpin.TabStop = true;
+            this.rbNumericSpin.Text = "Numeric Spin";
+            this.rbNumericSpin.UseVisualStyleBackColor = true;
+            // 
+            // rbProgressBar
+            // 
+            this.rbProgressBar.AutoSize = true;
+            this.rbProgressBar.Location = new System.Drawing.Point(17, 70);
+            this.rbProgressBar.Name = "rbProgressBar";
+            this.rbProgressBar.Size = new System.Drawing.Size(85, 17);
+            this.rbProgressBar.TabIndex = 1;
+            this.rbProgressBar.TabStop = true;
+            this.rbProgressBar.Text = "Progress Bar";
+            this.rbProgressBar.UseVisualStyleBackColor = true;
+            // 
+            // rbTrackBar
+            // 
+            this.rbTrackBar.AutoSize = true;
+            this.rbTrackBar.Location = new System.Drawing.Point(17, 107);
+            this.rbTrackBar.Name = "rbTrackBar";
+            this.rbTrackBar.Size = new System.Drawing.Size(72, 17);
+            this.rbTrackBar.TabIndex = 2;
+            this.rbTrackBar.TabStop = true;
+            this.rbTrackBar.Text = "Track Bar";
+            this.rbTrackBar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 278);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbTrackBar;
+        private System.Windows.Forms.RadioButton rbProgressBar;
+        private System.Windows.Forms.RadioButton rbNumericSpin;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Week 07/ProgressIndicator/ProgressIndicator/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgressIndicator
+{
+    public partial class Form1 : Form
+    {
+        FeedbackDelegate feedbackDelegate;  // make this guy available to everybody
+        public Form1()
+        {
+            InitializeComponent();
+            setUpControls();
+            rbNumericSpin.Checked = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            setUpControls();            // reset
+            if (rbTrackBar.Checked)
+                feedbackDelegate = new FeedbackDelegate(updateTrackBar);
+            else if (rbProgressBar.Checked)
+                feedbackDelegate = new FeedbackDelegate(updateProgressBar);
+            else
+                feedbackDelegate = new FeedbackDelegate(updateSpinBox);
+           
+            slowMethod(feedbackDelegate);   // call our slow method, who accepts a FeedBackDelegate
+        }
+
+        public delegate void FeedbackDelegate();
+
+        public void slowMethod(FeedbackDelegate feedbackDelegate)
+        {
+            // use sleep
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+                feedbackDelegate();
+            }
+        }
+        
+        // below methods will be used with our delegate - signatures match
+        public void updateSpinBox()
+        {
+            numericUpDown1.Value++;         
+            Application.DoEvents();     // have to call this or control won't update
+        }
+
+        public void updateProgressBar()
+        {
+            progressBar1.Value++;
+        }
+
+        public void updateTrackBar()
+        {
+            trackBar1.Value++;
+        }
+
+
+        // method to set up controls
+        public void setUpControls()
+        {
+            trackBar1.Maximum = 10;
+            trackBar1.Value = 0;
+            numericUpDown1.Value = 0;
+            numericUpDown1.Maximum = 10;
+            progressBar1.Value = 0;
+            progressBar1.Maximum = 10;
+        }
+
+    }
+}
