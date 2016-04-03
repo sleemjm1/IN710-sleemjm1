c7f6f5d9399efb417f47c73be1a0cca04005e142
Joe Sleeman <joes@op.ac.nz>
2016-04-03 16:20:43 +1200

    Started garden task

---
+++ Week 07/GardenReporter/GardenReporter2016/GardenManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GardenReporter2016
+{
+    public class GardenManager
+    {
+        public List<Garden> gardenList;
+
+        public GardenManager()
+        {
+            gardenList = new List<Garden>();
+        }
+        public void AddGarden(Garden garden)
+        {
+            gardenList.Add(garden);
+        }
+
+        public void ProcessGardens()
+        {
+            throw new NotImplementedException;
+        }
+    }
+}
