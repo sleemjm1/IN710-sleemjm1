72003344b3d7fa25679d1ea89eeae32bda1fe202
Joseph Sleeman (08003882) <SLEEMJM1@student.op.ac.nz>
2016-04-04 14:00:57 +1200

    Predicate task completed

---
+++ Week 07/Predicate/Predicate/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Predicate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnEven = new System.Windows.Forms.Button();
+            this.btnLessThanTen = new System.Windows.Forms.Button();
+            this.listGeneratedNumbers = new System.Windows.Forms.ListBox();
+            this.listSelectedNumbers = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(49, 45);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(178, 23);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate Randoms";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnEven
+            // 
+            this.btnEven.Location = new System.Drawing.Point(273, 45);
+            this.btnEven.Name = "btnEven";
+            this.btnEven.Size = new System.Drawing.Size(178, 23);
+            this.btnEven.TabIndex = 1;
+            this.btnEven.Text = "Select Even Numbers";
+            this.btnEven.UseVisualStyleBackColor = true;
+            this.btnEven.Click += new System.EventHandler(this.btnEven_Click);
+            // 
+            // btnLessThanTen
+            // 
+            this.btnLessThanTen.Location = new System.Drawing.Point(273, 90);
+            this.btnLessThanTen.Name = "btnLessThanTen";
+            this.btnLessThanTen.Size = new System.Drawing.Size(178, 23);
+            this.btnLessThanTen.TabIndex = 2;
+            this.btnLessThanTen.Text = "Select Numbers < 10";
+            this.btnLessThanTen.UseVisualStyleBackColor = true;
+            this.btnLessThanTen.Click += new System.EventHandler(this.btnLessThanTen_Click);
+            // 
+            // listGeneratedNumbers
+            // 
+            this.listGeneratedNumbers.FormattingEnabled = true;
+            this.listGeneratedNumbers.Location = new System.Drawing.Point(49, 138);
+            this.listGeneratedNumbers.Name = "listGeneratedNumbers";
+            this.listGeneratedNumbers.Size = new System.Drawing.Size(178, 459);
+            this.listGeneratedNumbers.TabIndex = 3;
+            // 
+            // listSelectedNumbers
+            // 
+            this.listSelectedNumbers.FormattingEnabled = true;
+            this.listSelectedNumbers.Location = new System.Drawing.Point(273, 138);
+            this.listSelectedNumbers.Name = "listSelectedNumbers";
+            this.listSelectedNumbers.Size = new System.Drawing.Size(178, 459);
+            this.listSelectedNumbers.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 637);
+            this.Controls.Add(this.listSelectedNumbers);
+            this.Controls.Add(this.listGeneratedNumbers);
+            this.Controls.Add(this.btnLessThanTen);
+            this.Controls.Add(this.btnEven);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
+            this.Text = "Predicate";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnEven;
+        private System.Windows.Forms.Button btnLessThanTen;
+        private System.Windows.Forms.ListBox listGeneratedNumbers;
+        private System.Windows.Forms.ListBox listSelectedNumbers;
+    }
+}
+
---
+++ Week 07/Predicate/Predicate/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Predicate
+{
+    public partial class Form1 : Form
+    {
+        List<int> randomList;
+        List<int> sortedList;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            generateRandoms();
+        }
+
+      
+        private void btnEven_Click(object sender, EventArgs e)
+        {
+            Predicate<int> isEvenPredicate = new Predicate<int>(isEven);
+            if (randomList == null)
+                MessageBox.Show("Please generate numbers before trying to sort");
+            else
+                sortList(isEvenPredicate, randomList);
+        }
+
+        
+
+        private void btnLessThanTen_Click(object sender, EventArgs e)
+        {
+            Predicate<int> isLessThanTenPredicate = new Predicate<int>(isLessThanTen);
+            if (randomList == null)
+                MessageBox.Show("Please generate numbers before trying to sort");
+            else
+                sortList(isLessThanTenPredicate, randomList);
+        }
+
+        private void generateRandoms()
+        {
+            randomList = new List<int>();
+            Random r = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                randomList.Add(r.Next(0, 99));  // number between 0-99
+            }
+
+            fillListBox(listGeneratedNumbers, randomList);
+        }
+
+        private void fillListBox(ListBox listBox, List<int> IntegerList)    // use whenever we want to fill a ListBox on form
+        {
+            listBox.Items.Clear();
+            foreach (int i in IntegerList)
+            {
+                listBox.Items.Add(i);
+            }
+        }
+
+        private void sortList(Predicate<int> sortingPredicate, List<int> listToBeSorted)    // accepts predicate & a list to be sorted
+        {
+            sortedList = listToBeSorted.FindAll(sortingPredicate);                          // place in list based on the sorting predicate passed in
+            fillListBox(listSelectedNumbers, sortedList);                                   // fill list on form
+        }
+
+        // methods to be used with our predicate
+        private bool isEven(int inputInt)
+        {
+            bool isAnEvenNumber = ((inputInt % 2) == 0);    // mod 2 = 0 means even number 
+            return isAnEvenNumber;
+        }
+
+        private bool isLessThanTen(int inputInt)
+        {
+            bool isLessThanTen = (inputInt < 10);           // if our input int < 10
+            return isLessThanTen;
+        }
+
+      
+    }
+}
