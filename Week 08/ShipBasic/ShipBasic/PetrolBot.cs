345861e8acaae087ba606b2898c14ef9726fa92b
Joe Sleeman <sleemanjm@gmail.com>
2016-04-07 16:24:41 +1200

    Started skeleton for Ship task

---
+++ Week 08/ShipBasic/ShipBasic/PetrolBot.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipBasic
+{
+    public class PetrolBot
+    {
+        Graphics botCanvas;
+        Color botColor;
+
+        Ship botShip; // Implement Ship class...
+
+        Point botCurrentLocation;
+        Point botStartingLocation;
+        Point shipLocation;
+
+        public PetrolBot()  // Const. Probably need params
+        {
+            throw new NotImplementedException();
+        }
+
+        public void drawBot()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FullOfFuelEventHandler()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OutOfFuelEventHandler()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ Week 08/ShipBasic/ShipBasic/Ship.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipBasic
+{
+    public class Ship
+    {
+        // Fields
+        int petrol;
+        Random rGen;
+        Graphics shipCanvas;
+        Color shipColor;
+        Point shipLocation;
+        int shipSize;
+        int shipVelocity;
+        // shipState --- ENUM?????
+
+        // Properties
+        public int Petrol { get; set; }
+        public Point shipLocation { get; set; }
+
+        // Constructor
+        public void Ship()  // Maybe need params not sure..
+        {
+            throw new NotImplementedException();
+        }
+
+        // Methods
+        public void drawShip()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void moveShip()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnFullOfFuelEvent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnOutOfFuelEvent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void refuel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShipCycle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void usePetrol()
+        {
+            throw new NotImplementedException();
+        }
+        
+
+        // Need to put events in to skeleton 
+
+
+    }
+}
